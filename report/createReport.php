9fef8e584d0c1babc5f30c5a1a606c7c4db5f0c4
dvdunguet <dvdunguet@gmail.com>
2018-05-06 13:27:04 +0700

    phien ban 1.1

--- report/createReport.php
+++ report/createReport.php
@@ -8,15 +8,21 @@ $hostname=$_POST['hostname'];
 $url='http://localhost/visualization/report/';
 
 $pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(0,10,"Report of $hostname",0,0,'C');
 $i=0;
 foreach ($items as $value){
     $url="$url"."createImage.php?itemid=$value&startDate=$startDate&endDate=$endDate#.jpg";
-    if($i%2==0) {
+    if($i==0){
+        $pdf->Image("$url",5,40,200,100);
+    }elseif($i%2==0) {
         $pdf->AddPage();
-        $pdf->Image("$url",5,30,200,100);
+        $pdf->Image("$url",5,40,200,100);
     }else
-    $pdf->Image("$url",5,180,200,100);
+    $pdf->Image("$url",5,150,200,100);
     $i++;
 }
-$pdf->Output("report_$hostname.pdf",'D')
+$pdf->Output("report_$hostname.pdf",'I');
+
 ?>
